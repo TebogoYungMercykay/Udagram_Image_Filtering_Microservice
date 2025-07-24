e7036f79a1ad1df1ba2211996ee2eceee288d27d
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2025-07-25 00:03:25 +0200

    (src) Latest Server and Utilities in TypeScript

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,37 +1,44 @@
 import fs from "fs";
-import Jimp = require("jimp");
+import * as Jimp from "jimp";
 
-// filterImageFromURL
-// helper function to download, filter, and save the filtered image locally
-// returns the absolute path to the local image
-// INPUTS
-//    inputURL: string - a publicly accessible url to an image file
-// RETURNS
-//    an absolute path to a filtered image locally saved file
+/**
+ * filterImageFromURL
+ * Helper function to download, filter, and save the filtered image locally
+ * Returns the absolute path to the local image
+ * 
+ * @param inputURL - A publicly accessible URL to an image file
+ * @returns An absolute path to a filtered image locally saved file
+ */
 export async function filterImageFromURL(inputURL: string): Promise<string> {
   return new Promise((resolve, reject) => {
-      Jimp.read(inputURL).then(photo => {
-          let outpath: string = '/tmp/filtered.' + Math.floor(Math.random() * 2000) + '.jpg';
-          photo
-              .resize(256, 256) // resize
-              .quality(60) // set JPEG quality
-              .greyscale() // set greyscale
-              .write(__dirname + outpath, (img) => {
-                  resolve(__dirname + outpath);
-              });
-      }).catch(err => {
-          console.error(err);
-          reject("Could not read image.");
+    Jimp.read(inputURL)
+      .then(photo => {
+        const outpath: string = '/tmp/filtered.' + Math.floor(Math.random() * 2000) + '.jpg';
+        
+        photo
+          .resize(256, 256) // resize
+          .quality(60) // set JPEG quality
+          .greyscale() // set greyscale
+          .write(__dirname + outpath, (img) => {
+            resolve(__dirname + outpath);
+          });
       })
+      .catch(err => {
+        console.error(err);
+        reject("Could not read image.");
+      });
   });
 }
-// deleteLocalFiles
-// helper function to delete files on the local disk
-// useful to cleanup after tasks
-// INPUTS
-//    files: Array<string> an array of absolute paths to files
-export async function deleteLocalFiles(files: Array<string>) {
-  for (let file of files) {
+
+/**
+ * deleteLocalFiles
+ * Helper function to delete files on the local disk
+ * Useful to cleanup after tasks
+ * 
+ * @param files - An array of absolute paths to files
+ */
+export async function deleteLocalFiles(files: Array<string>): Promise<void> {
+  for (const file of files) {
     fs.unlinkSync(file);
   }
 }
